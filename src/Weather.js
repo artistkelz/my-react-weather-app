776eec7fed47d0ba15b01f8c864740ddc939c767
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-04-12 18:46:32 -0700

    Creating react weather app

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${props.city} is ${response.data.main.temp}°C`);
+  }
+  let apiKey = "44c433b8d9236adacf244d61ee2e704d";
+  let apiURL = `https://api.openweather.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(handleResponse);
+  return <h2>Hello from Weather</h2>;
+}
